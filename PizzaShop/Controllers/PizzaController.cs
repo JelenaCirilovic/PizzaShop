6a210f54b00bcff486010f4964ba51f80931df40
Jelena Cirilovic <jelenacirilovic@gmail.com>
2024-03-05 14:03:45 +0100

    napraviti listu ali nisam uspela

---
+++ PizzaShop/Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Models;
+
+namespace PizzaShop.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.uslov = false;
+            ViewBag.Message = "Ovo je poruka sa servera";
+            ViewBag.Mesage2 = "Ovo je druga poruka";
+            return View();
+            
+        }
+        private IPieRepository _repository;
+
+        public PizzaController(IPieRepository repository) 
+        { 
+            _repository=repository;
+        }
+
+
+        public ViewResult List()
+        {
+            return View(_repository.Pies);
+        }
+    }
+
+}
